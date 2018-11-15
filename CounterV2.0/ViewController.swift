5d8b6d9fa25efda13e3d562d0a351948694c77fe
Git1Sunny <764152162@qq.com>
2018-11-15 11:55:12 +0800

    New01
    New

--- CounterV2.0/ViewController.swift
+++ CounterV2.0/ViewController.swift
@@ -9,7 +9,61 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    class NumberData{
+        var Num = [Int]()
+        var Top : Int = 0
+    }
+    class CountData{
+        var Cou = [Character]()
+        var Top : Int = 0
+    }
+    var MainScreentext : String = ""
+    var Numbertext : String = ""
+    var Counttext : String = ""
+    func JudgeCount(Text:String) -> Bool
+    {
+        if(Text == "+"||Text == "-"||Text == "x"||Text == "÷"){
+            return true
+        }
+        return false
+    }
+    func reNumberData(data : NumberData)
+    {
+        data.Top = -1
+    }
+    func reCountData(data : CountData)
+    {
+        data.Top = -1
+    }
+    
+    @IBOutlet weak var MainScreen: UILabel!
+    @IBOutlet weak var ScendScreen: UILabel!
+    @IBAction func NumberButton(_ sender: UIButton)
+    {
+        MainScreentext += sender.currentTitle!
+        MainScreen.text = MainScreentext
+        Numbertext += sender.currentTitle!
+    }
+    @IBAction func CountButton(_ sender: UIButton)
+    {
+        if(JudgeCount( Text: (String(MainScreen.text!.suffix(1)))))
+        {
+            MainScreentext.remove(at: MainScreentext.index(before: MainScreentext.endIndex))
+        }
+        MainScreentext += sender.currentTitle!
+        MainScreen.text = MainScreentext
+    }
+    @IBAction func KuoHaoButton(_ sender: Any) {
+    }
+    @IBAction func PointButton(_ sender: Any) {
+    }
+    @IBAction func Change(_ sender: Any) {
+    }
+    @IBAction func DeleteNumber(_ sender: Any) {
+    }
+    @IBAction func ACButton(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
