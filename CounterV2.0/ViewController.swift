9f6658a32f4df65b7fd70274243dc9e8eb148db3
Git1Sunny <764152162@qq.com>
2018-11-16 21:36:57 +0800

    Updata 11.16
    Updata

--- CounterV2.0/ViewController.swift
+++ CounterV2.0/ViewController.swift
@@ -10,13 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
     class NumberData{
-        var Num = [Int]()
+        var Num = [Double](repeating: 0, count: 20)
         var Top : Int = 0
+        func PushNum(inNum:Double)
+        {
+            self.Top += 1
+            self.Num[self.Top] = inNum
+
+        }
+        func PopNum() -> Double
+        {
+            var data : Double = 0
+            data = self.Num[self.Top]
+            self.Top -= 1
+            return data
+        }
+        func reNumberData()
+        {
+            self.Top = -1
+        }
     }
     class CountData{
-        var Cou = [Character]()
+        var Cou = [String](repeating: "o", count: 20)
         var Top : Int = 0
+        func reCountData()
+        {
+            self.Top = -1
+        }
+        func PopCou() -> String
+        {
+            var data : String
+            data = self.Cou[self.Top]
+            self.Top -= 1
+            return data
+        }
+        func PushCou(inCou : String)
+        {
+            self.Top += 1
+            self.Cou[self.Top] = inCou
+        }
     }
+    var NumData = NumberData()
+    var CouData = CountData()
+    var KuohaoNumber : Int = 0
     var MainScreentext : String = ""
     var Numbertext : String = ""
     var Counttext : String = ""
@@ -27,22 +63,19 @@ class ViewController: UIViewController {
         }
         return false
     }
-    func reNumberData(data : NumberData)
-    {
-        data.Top = -1
-    }
-    func reCountData(data : CountData)
-    {
-        data.Top = -1
-    }
     
     @IBOutlet weak var MainScreen: UILabel!
     @IBOutlet weak var ScendScreen: UILabel!
     @IBAction func NumberButton(_ sender: UIButton)
     {
+        if(MainScreen.text!.count>=9)
+        {
+            MainScreentext += "\n"
+        }
         MainScreentext += sender.currentTitle!
         MainScreen.text = MainScreentext
         Numbertext += sender.currentTitle!
+        print(Numbertext)
     }
     @IBAction func CountButton(_ sender: UIButton)
     {
@@ -52,16 +85,73 @@ class ViewController: UIViewController {
         }
         MainScreentext += sender.currentTitle!
         MainScreen.text = MainScreentext
+        NumData.PushNum(inNum: Double(Numbertext)!)
+        Numbertext = ""
     }
     @IBAction func KuoHaoButton(_ sender: Any) {
+        if(String(MainScreen.text!.suffix(1)) == ".")
+        {
+            MainScreentext.remove(at: MainScreentext.index(before: MainScreentext.endIndex))
+        }
+        if(JudgeCount( Text: (String(MainScreen.text!.suffix(1)))))
+        {
+            MainScreentext += "("
+        }
+        else
+        {
+            switch String(MainScreen.text!.suffix(1)) {
+            case "(":
+                MainScreentext += "("
+                KuohaoNumber += 1
+                break;
+            case ")":
+                if(KuohaoNumber == 0)
+                {
+                    MainScreentext += "x("
+                    KuohaoNumber += 1
+                }
+                else
+                {
+                    MainScreentext += ")"
+                    KuohaoNumber -= 1
+                }
+                break
+            default:
+                if(KuohaoNumber == 0)
+                {
+                    MainScreentext += "x("
+                    KuohaoNumber += 1
+                }
+                else
+                {
+                    MainScreentext += ")"
+                    KuohaoNumber -= 1
+                }
+            }
+        }
+        MainScreen.text = MainScreentext
     }
     @IBAction func PointButton(_ sender: Any) {
+        if(!Numbertext.contains("."))
+        {
+            Numbertext += "."
+            MainScreentext += "."
+            MainScreen.text! = MainScreentext
+        }
     }
     @IBAction func Change(_ sender: Any) {
     }
     @IBAction func DeleteNumber(_ sender: Any) {
     }
     @IBAction func ACButton(_ sender: Any) {
+        KuohaoNumber = 0
+        MainScreentext = ""
+        MainScreen.text = "0"
+        Counttext = ""
+        Numbertext = ""
+        ScendScreen.text = ""
+        NumData.reNumberData()
+        CouData.reCountData()
     }
     
     override func viewDidLoad() {
