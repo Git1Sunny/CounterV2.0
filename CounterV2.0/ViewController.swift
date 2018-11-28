7132e4b74420bc6c5b4f473d51fcee664bf4f042
Git1Sunny <764152162@qq.com>
2018-11-28 10:04:10 +0800

    2018.11.28 Updata
    增加队列

--- CounterV2.0/ViewController.swift
+++ CounterV2.0/ViewController.swift
@@ -50,14 +50,40 @@ class ViewController: UIViewController {
             self.Cou[self.Top] = inCou
         }
     }
+    class StdinData{
+        var Stdin = [String](repeating: "=", count: 29)
+        var Top : Int = 0
+        var End : Int = 0
+        var Count : Int = 0
+        func reStdinData()
+        {
+            self.Top = 0
+            self.End = 0
+        }
+        func PopStd() -> String
+        {
+            var data : String
+            data = self.Stdin[self.Top]
+            self.Top = (self.Top+1)%29
+            Count -= 1
+            return data
+        }
+        func PushStd(inStd : String)
+        {
+            self.Stdin[self.End] = inStd
+            self.End = (self.End+1)%29
+            Count += 1
+        }
+    }
     var NumData = NumberData()
     var CouData = CountData()
+    var StdData = StdinData()
     var KuohaoNumber : Int = 0
     var MainScreentext : String = ""
     var Numbertext : String = ""
     var Counttext : String = ""
     func JudgeCountAll(Text:String) -> Bool {
-        if(Text == "+"||Text == "-"||Text == "x"||Text == "÷"||Text == "("||Text == ")")
+        if(Text == "+"||Text == "-"||Text == "x"||Text == "÷"||Text == "("||Text == ")"||Text == "=")
         {
             return true
         }
@@ -68,14 +94,14 @@ class ViewController: UIViewController {
         var Number : String = ""
         for ch in Text.characters
         {
-            if(JudgeCount(Text: String(ch)))
+            if(JudgeCountAll(Text: String(ch)))
             {
-                CouData.PushCou(inCou: String(ch))
                 if(!Number.isEmpty)
                 {
-                    NumData.PushNum(inNum: Double(Number)!)
+                    StdData.PushStd(inStd: Number)
                     Number = ""
                 }
+                StdData.PushStd(inStd: String(ch))
             }
             else
             {
@@ -84,7 +110,7 @@ class ViewController: UIViewController {
         }
         if(!Number.isEmpty)
         {
-            NumData.PushNum(inNum: Double(Number)!)
+            StdData.PushStd(inStd: Number)
             Number = ""
         }
     }
@@ -143,20 +169,13 @@ class ViewController: UIViewController {
                 Text = "<"
                 break
             case ")":
-                    Text = ">"
+                Text = ")"
                 break
-            /*case "=":
-                switch Text1
-                {
-                case "=":
-                    Text = "="
-                    break
-                default:
-                    Text = ">"
-                }
-                break*/
+            case "=":
+                        Text = ">"
+                break
             default:
-                break
+                print("Error --2")
             }
         return Text
     }
@@ -187,7 +206,48 @@ class ViewController: UIViewController {
         }
         return Result
     }
-    
+    func FinalCount()
+    {
+        var sign : String = ""
+        var Num1 : Double = 0
+        var Num2 : Double = 0
+        var result : Double = 0
+        var ch : String = ""
+        while  StdData.Count != 0 {
+            ch = StdData.PopStd()
+            if(!JudgeCountAll(Text: ch))
+            {
+                NumData.PushNum(inNum: Double(ch)!)
+            }
+            else
+            {
+                 switch(CampCount(Text1: CouData.Cou[CouData.Top], Text2:ch))
+                 {
+                 case "<":
+                    CouData.PushCou(inCou: ch)
+                    break
+                 case ">":
+                    sign = CouData.PopCou()
+                    Num2 = NumData.PopNum()
+                    Num1 = NumData.PopNum()
+                    result = MainCount(Num1: Num1, Cou: sign, Num2: Num2)
+                    NumData.PushNum(inNum: result)
+                    break
+                 case ")":
+                    sign = CouData.PopCou()
+                    Num2 = NumData.PopNum()
+                    Num1 = NumData.PopNum()
+                    result = MainCount(Num1: Num1, Cou: sign, Num2: Num2)
+                    NumData.PushNum(inNum: result)
+                    CouData.PopCou()
+                 default:
+                    print("Number Button Error")
+                 break
+                 }
+            }
+        }
+        //  循环运算 全部数据 可以改上面的循环
+    }
     @IBOutlet weak var MainScreen: UILabel!
     @IBOutlet weak var ScendScreen: UILabel!
     @IBAction func NumberButton(_ sender: UIButton)
@@ -334,21 +394,16 @@ class ViewController: UIViewController {
     }
     @IBAction func Change(_ sender: Any) {
         CutScreen(Text: MainScreen.text!)
-        
+        FinalCount()
+        print(NumData.Num[NumData.Top])
     }
     @IBAction func DeleteNumber(_ sender: Any) {
-        if(MainScreen.text!.count == 1) //如果回格最后一个数字 重置为0
-        {
-            MainScreen.text = "0"
-            MainScreentext = ""
-        }
-        if(MainScreen.text! != "0"){ //若值为0 无法回格
-            if(JudgeCount(Text: String(MainScreen.text!.suffix(1))))
-            {
-                MainScreen.text!.remove(at: MainScreen.text!.index(before: MainScreen.text!.endIndex)) //删除最后一位
-                MainScreentext = MainScreen.text!
-            }
-        }
+        StdData.PushStd(inStd: "1")
+        StdData.PushStd(inStd: "2")
+        StdData.PushStd(inStd: "3")
+        print(StdData.PopStd())
+        print(StdData.PopStd())
+        print(StdData.PopStd())
     }
     @IBAction func ACButton(_ sender: Any) {
         KuohaoNumber = 0
@@ -359,6 +414,7 @@ class ViewController: UIViewController {
         ScendScreen.text = ""
         NumData.reNumberData()
         CouData.reCountData()
+        StdData.reStdinData()
     }
     
     @IBAction func FinalCount(_ sender: Any) {
